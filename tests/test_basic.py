69c18ea0f99121cdb0f84fb4c6102d558a0aa72f
Max Hacking <max@hacking.co.uk>
2020-02-12 19:33:52 +0000

    Restore tests now test ebuilds are in the (hacking-gentoo) overlay.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -57,42 +57,40 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(kwd1["alpha"], "~alpha")
         self.assertEqual(kwd1["x86"], "x86")
 
-#===============================================================================
-#     def test_parse_atom_dep_a(self):
-#         CONFIG['verbose']=False
-#         kwd=parse_atom("dev-test/dep-a")
-#         self.assertEqual(kwd["alpha"], "alpha")
-#         self.assertEqual(kwd["arm"], "arm")
-#         self.assertEqual(kwd["x86"], "x86")
-#         
-#     def test_parse_atom_dep_b(self):
-#         CONFIG['verbose']=False
-#         kwd=parse_atom("dev-test/dep-b")
-#         self.assertEqual(kwd["alpha"], "~alpha")
-#         self.assertEqual(kwd["arm"], "arm")
-#         self.assertEqual(kwd["x86"], "x86")
-#         
-#     def test_parse_atom_dep_c(self):
-#         CONFIG['verbose']=False
-#         kwd=parse_atom("dev-test/dep-c")
-#         self.assertEqual(kwd["alpha"], "~alpha")
-#         self.assertEqual(kwd["arm"], "~arm")
-#         self.assertEqual(kwd["x86"], "x86")
-#         
-#     def test_parse_list_cf(self):
-#         CONFIG['verbose']=False
-#         kwd=parse_list(["dev-test/dep-a", "dev-test/dep-b", "dev-test/dep-c"], False)
-#         self.assertEqual(kwd["alpha"], "~alpha")
-#         self.assertEqual(kwd["arm"], "~arm")
-#         self.assertEqual(kwd["x86"], "x86")
-# 
-#     def test_parse_list_ct(self):
-#         CONFIG['verbose']=False
-#         kwd=parse_list(["dev-test/dep-a", "dev-test/dep-b", "dev-test/dep-c"], True)
-#         self.assertEqual(kwd["alpha"], "alpha")
-#         self.assertEqual(kwd["arm"], "arm")
-#         self.assertEqual(kwd["x86"], "x86")
-#===============================================================================
+    def test_parse_atom_dep_a(self):
+        CONFIG['verbose']=False
+        kwd=parse_atom("dev-test/dep-a")
+        self.assertEqual(kwd["alpha"], "alpha")
+        self.assertEqual(kwd["arm"], "arm")
+        self.assertEqual(kwd["x86"], "x86")
+         
+    def test_parse_atom_dep_b(self):
+        CONFIG['verbose']=False
+        kwd=parse_atom("dev-test/dep-b")
+        self.assertEqual(kwd["alpha"], "~alpha")
+        self.assertEqual(kwd["arm"], "arm")
+        self.assertEqual(kwd["x86"], "x86")
+         
+    def test_parse_atom_dep_c(self):
+        CONFIG['verbose']=False
+        kwd=parse_atom("dev-test/dep-c")
+        self.assertEqual(kwd["alpha"], "~alpha")
+        self.assertEqual(kwd["arm"], "~arm")
+        self.assertEqual(kwd["x86"], "x86")
+         
+    def test_parse_list_cf(self):
+        CONFIG['verbose']=False
+        kwd=parse_list(["dev-test/dep-a", "dev-test/dep-b", "dev-test/dep-c"], False)
+        self.assertEqual(kwd["alpha"], "~alpha")
+        self.assertEqual(kwd["arm"], "~arm")
+        self.assertEqual(kwd["x86"], "x86")
+ 
+    def test_parse_list_ct(self):
+        CONFIG['verbose']=False
+        kwd=parse_list(["dev-test/dep-a", "dev-test/dep-b", "dev-test/dep-c"], True)
+        self.assertEqual(kwd["alpha"], "alpha")
+        self.assertEqual(kwd["arm"], "arm")
+        self.assertEqual(kwd["x86"], "x86")
 
 if __name__ == '__main__':
     unittest.main()
